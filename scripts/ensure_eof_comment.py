b6ae03d3a288fd27d4cc3eb8b12ecf85afbb2280
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-12-08 20:10:17 -0600

    feat(ci): add script to ensure EOF Vim comment in files
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

---
+++ scripts/ensure_eof_comment.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Ensure EOF Vim comment in Lua files.
+
+Copyright (c) 2025 Guennadi Maximov C. All Rights Reserved.
+
+Usage: python3 ensure_eof_comment.py
+"""
+from io import TextIOWrapper
+from os import walk
+from os.path import join
+from sys import exit as Exit
+from sys import stderr as STDERR
+from typing import Dict, List, NoReturn, Tuple, Union
+
+COMMENT: str = "-- vim:ts=2:sts=2:sw=2:et:ai:si:sta:"
+
+
+def bootstrap_paths() -> Tuple[str]:
+    """Bootstraps all the matching paths in current dir and below."""
+    result = list()
+    for path in ("./lua", "./plugin", "./spec"):
+        for root, dirs, files in walk(path):
+            for file in files:
+                if file.endswith(".lua"):
+                    result.append(join(root, file))
+
+    return tuple(result)
+
+
+def die(*msg, code: int = 0) -> NoReturn:
+    """Kill program execution."""
+    if code == 0:
+        func = print
+    else:
+        func = error
+
+    func(*msg)
+    Exit(code)
+
+
+def error(*msg, end: str = "\n", sep: str = " ", flush: bool = False) -> NoReturn:
+    """Prints to stderr."""
+    print(*msg, end=end, sep=sep, flush=flush, file=STDERR)
+
+
+def open_batch_paths(paths: Tuple[str]) -> Dict[str, TextIOWrapper]:
+    """Return a list of TextIO objects given file path strings."""
+    result = dict()
+    for path in paths:
+        try:
+            result[path] = open(path, "r")
+
+        except FileNotFoundError:
+            error(f"File `{path}` is not available!")
+            continue
+
+        except KeyboardInterrupt:
+            die("\nProgram interrupted!", code=1)  # Kills the program
+
+        except Exception:
+            error(f"Something went wrong while trying to open `{path}`!")
+            continue
+
+    return result
+
+
+def get_last_line(file: TextIOWrapper) -> str:
+    """Returns the last line of a file."""
+    result = file.read().split("\n")[-2]
+    file.close()
+
+    return result
+
+
+def eof_comment_search(
+        files: Dict[str, TextIOWrapper]
+) -> Dict[str, List[Union[TextIOWrapper, bool]]]:
+    """Searches through opened files."""
+    result = dict()
+    for path, file in files.items():
+        last_line = get_last_line(file)
+        if last_line not in (COMMENT,):
+            if last_line in ("-" + COMMENT, COMMENT.split(" "), "-" + "".join(COMMENT.split(" "))):
+                result[path] = [open(path, "r"), True]
+            else:
+                result[path] = [open(path, "a"), False]
+
+    return result
+
+
+def modify_file(file: TextIOWrapper) -> str:
+    """Modifies a file containing a bad EOF comment."""
+    data = file.read().split("\n")
+    data[-2] = COMMENT
+    data.insert(-2, "")  # Newline
+
+    return "\n".join(data)
+
+
+def append_eof_comment(files: Dict[str, List[Union[TextIOWrapper, bool]]]) -> NoReturn:
+    """Append EOF comment to files missing it."""
+    for path, file in files.items():
+        txt = f"{COMMENT}\n"
+        if file[1]:
+            txt = modify_file(file[0])
+            file[0] = open(path, "w")
+
+        file[0].write(txt)
+        file[0].close()
+
+
+def main() -> int:
+    """Execute main workflow."""
+    files = open_batch_paths(bootstrap_paths())
+
+    if len(files) == 0:
+        error("No matching files found!")
+        return 1
+
+    results = eof_comment_search(files)
+
+    if len(results) > 0:
+        append_eof_comment(results)
+
+    return 0
+
+
+if __name__ == "__main__":
+    Exit(main())
