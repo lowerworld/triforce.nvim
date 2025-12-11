884877fb944e506e6427257ec8da1c22bc8bb190
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-12-10 20:04:48 -0600

    refactor(ci)!: massively improved EOF checker
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- scripts/ensure_eof_comment.py
+++ scripts/ensure_eof_comment.py
@@ -6,14 +6,54 @@ Copyright (c) 2025 Guennadi Maximov C. All Rights Reserved.
 
 Usage: python3 ensure_eof_comment.py
 """
+from argparse import ArgumentError, ArgumentParser, Namespace
 from io import TextIOWrapper
 from os import walk
-from os.path import join
+from os.path import isdir, join
 from sys import exit as Exit
 from sys import stderr as STDERR
-from typing import Dict, List, NoReturn, Tuple, Union
+from sys import stdout as STDOUT
+from typing import Any, Dict, List, NoReturn, Tuple, Union
 
-COMMENT: str = "-- vim:ts=2:sts=2:sw=2:et:ai:si:sta:"
+COMMENTS: Dict[str, str] = {
+    # C
+    "c": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "h": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+
+    # C++
+    "cc": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "c++": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "cpp": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "C": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "hh": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "h++": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "hpp": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+    "H": "/// vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+
+    # Lua
+    "lua": "-- vim:ts=2:sts=2:sw=2:et:ai:si:sta:",
+
+    # Markdown
+    "md": "<!--\nvim:ts=2:sts=2:sw=2:et:ai:si:sta:\n-->",
+    "markdown": "<!--\nvim:ts=2:sts=2:sw=2:et:ai:si:sta:\n-->",
+
+    # HTML
+    "html": "<!--\nvim:ts=2:sts=2:sw=2:et:ai:si:sta:\n-->",
+    "htm": "<!--\nvim:ts=2:sts=2:sw=2:et:ai:si:sta:\n-->",
+
+    # CSS
+    "css": "/* vim:ts=2:sts=2:sw=2:et:ai:si:sta: */",
+
+    # Python
+    "py": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+    "pyi": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+
+    # Shell
+    "sh": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+    "bash": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+    "fish": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+    "zsh": "# vim:ts=4:sts=4:sw=4:et:ai:si:sta:",
+}
 
 
 def error(*msg, end: str = "\n", sep: str = " ", flush: bool = False) -> NoReturn:
@@ -76,29 +116,34 @@ def die(*msg, code: int = 0, end: str = "\n", sep: str = " ", flush: bool = Fals
     Exit(code)
 
 
-def bootstrap_paths() -> Tuple[str]:
+def bootstrap_paths(paths: Tuple[str], exts: Tuple[str]) -> Tuple[Tuple[str, str]]:
     """Bootstraps all the matching paths in current dir and below."""
     result = list()
-    for root, dirs, files in walk("./lua"):
-        for file in files:
-            if file.endswith(".lua"):
-                result.append(join(root, file))
+    for path in paths:
+        if not isdir(path):
+            continue
+
+        for root, dirs, files in walk(path):
+            for file in files:
+                for ext in exts:
+                    if file.endswith(ext):
+                        result.append((join(root, file), ext))
 
     return tuple(result)
 
 
-def open_batch_paths(paths: Tuple[str]) -> Dict[str, TextIOWrapper]:
+def open_batch_paths(paths: Tuple[Tuple[str, str]]) -> Dict[str, Tuple[TextIOWrapper, str]]:
     """Return a list of TextIO objects given file path strings."""
     result = dict()
     for path in paths:
         try:
-            result[path] = open(path, "r")
+            result[path[0]] = (open(path[0], "r"), path[1])
         except KeyboardInterrupt:
             die("\nProgram interrupted!", code=1)  # Kills the program
         except FileNotFoundError:
-            error(f"File `{path}` is not available!")
+            error(f"File `{path[0]}` is not available!")
         except Exception:
-            error(f"Something went wrong while trying to open `{path}`!")
+            error(f"Something went wrong while trying to open `{path[0]}`!")
 
     return result
 
@@ -112,45 +157,100 @@ def get_last_line(file: TextIOWrapper) -> str:
 
 
 def eof_comment_search(
-        files: Dict[str, TextIOWrapper]
-) -> Dict[str, List[Union[TextIOWrapper, bool]]]:
+        files: Dict[str, Tuple[TextIOWrapper, str]]
+) -> Dict[str, Tuple[Tuple[TextIOWrapper, bool], str]]:
     """Searches through opened files."""
     result = dict()
     for path, file in files.items():
-        last_line = get_last_line(file)
-        if last_line not in (COMMENT,):
-            if last_line in ("-" + COMMENT, COMMENT.split(" "), "-" + "".join(COMMENT.split(" "))):
-                result[path] = [open(path, "r"), True]
+        last_line = get_last_line(file[0])
+        comment = COMMENTS[file[1]]
+        if last_line not in (COMMENTS[file[1]],):
+            if last_line in ("-" + comment, comment.split(" "), "-" + "".join(comment.split(" "))):
+                result[path] = ([open(path, "r"), True], file[1])
             else:
-                result[path] = [open(path, "a"), False]
+                result[path] = ([open(path, "a"), False], file[1])
 
     return result
 
 
-def modify_file(file: TextIOWrapper) -> str:
+def modify_file(file: TextIOWrapper, ext: str) -> str:
     """Modifies a file containing a bad EOF comment."""
     data = file.read().split("\n")
-    data[-2] = COMMENT
-    data.insert(-2, "")  # Newline
+    data[-2] = COMMENTS[ext]
+    # data.insert(-2, "")  # Newline
 
     return "\n".join(data)
 
 
-def append_eof_comment(files: Dict[str, List[Union[TextIOWrapper, bool]]]) -> NoReturn:
+def append_eof_comment(files: Dict[str, Tuple[Tuple[TextIOWrapper, bool], str]]) -> NoReturn:
     """Append EOF comment to files missing it."""
     for path, file in files.items():
-        txt = f"{COMMENT}\n"
-        if file[1]:
-            txt = modify_file(file[0])
-            file[0] = open(path, "w")
+        txt = f"{COMMENTS[file[1]]}\n"
+        if file[0][1]:
+            txt = modify_file(file[0][0], file[1])
+            file[0][0] = open(path, "w")
 
-        file[0].write(txt)
-        file[0].close()
+        file[0][0].write(txt)
+        file[0][0].close()
+
+
+def bootstrap_args(
+        parser: ArgumentParser,
+        specs: Tuple[Tuple[List[str], Dict[str, Any]]]
+) -> Namespace:
+    """Bootstraps the program arguments."""
+    for spec in specs:
+        parser.add_argument(*spec[0], **spec[1])
+
+    try:
+        namespace = parser.parse_args()
+    except KeyboardInterrupt:
+        die(code=130)
+    except ArgumentError:
+        parser.print_help(STDOUT)
+        die(code=1)
+
+    return namespace
+
+
+def arg_parser_init() -> Tuple[ArgumentParser, Namespace]:
+    """Generates the argparse namespace."""
+    parser = ArgumentParser(
+        prog="ensure_eof_comment.py",
+        description="Checks for Vim EOF comments in all matching files in specific directories",
+        exit_on_error=False
+    )
+    spec = [
+        (
+            ["directories"],
+            {
+                "nargs": "+",
+                "help": "The target directories to be checked",
+                "metavar": "/path/to/directory",
+            },
+        ),
+        (
+            ["-e", "--file-extensions"],
+            {
+                "required": True,
+                "metavar": "EXT1[,EXT2[,EXT3[,...]]]",
+                "help": "A comma-separated list of file extensions (e.g. \"lua,c,cpp,cc,c++\")",
+                "dest": "exts",
+            }
+        ),
+    ]
+
+    return parser, bootstrap_args(parser, spec)
 
 
 def main() -> int:
     """Execute main workflow."""
-    files = open_batch_paths(bootstrap_paths())
+    parser, namespace = arg_parser_init()
+
+    dirs: Tuple[str] = tuple(namespace.directories)
+    exts: Tuple[str] = tuple(namespace.exts.split(","))
+
+    files = open_batch_paths(bootstrap_paths(dirs, exts))
     if len(files) == 0:
         error("No matching files found!")
         return 1
