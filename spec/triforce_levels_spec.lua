ea2a0d2839b28f77084c78b1a702b0d6bda88b0b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-05 12:43:21 -0600

    test(levels): add `levels` test spec (#36)

---
+++ spec/triforce_levels_spec.lua
@@ -0,0 +1,29 @@
+describe('triforce', function()
+  local triforce ---@type Triforce
+  local levels ---@type Triforce.Levels
+
+  before_each(function()
+    -- Clear module cache to get fresh instance
+    package.loaded.triforce = nil
+    triforce = require('triforce')
+    levels = require('triforce.levels')
+  end)
+
+  describe('levels', function()
+    it('should parse custom levels', function()
+      local lvls = { ---@type LevelParams[]
+        { level = 3, title = 'Test 1' },
+        { level = 5, title = 'Test 2' },
+        { level = 8, title = 'Test 3' },
+      }
+
+      local ok = pcall(triforce.setup, { levels = lvls })
+      assert.is_true(ok)
+
+      for _, lvl in ipairs(lvls) do
+        assert.is_same((lvl.icon or '') .. ' ' .. lvl.title, levels.get_level_title(lvl.level))
+      end
+    end)
+  end)
+end)
+-- vim: set ts=2 sts=2 sw=2 et ai si sta:
