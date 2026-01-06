f6a903b9c6d61c7eb02f172f34c167ffcb710213
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-06 11:43:37 -0600

    test(achievement): new unit tests
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- spec/triforce_achievement_spec.lua
+++ spec/triforce_achievement_spec.lua
@@ -2,17 +2,18 @@ local assert = require('luassert') ---@type Luassert
 
 describe('triforce', function()
   local triforce ---@type Triforce
+  local achievement ---@type Triforce.Achievements
 
   before_each(function()
     -- Clear module cache to get fresh instance
     package.loaded.triforce = nil
     triforce = require('triforce')
+    achievement = require('triforce.achievement')
     triforce.setup()
   end)
 
   describe('achievement', function()
     it('should accept new achievements', function()
-      local achievement = require('triforce.achievement')
       local get_stats = require('triforce.tracker').get_stats
       local achv = {
         id = 'test_achv_1',
@@ -26,11 +27,43 @@ describe('triforce', function()
       ok, stats = pcall(get_stats)
       assert.is_true(ok)
 
-      ok = pcall(achievement.new_achievements, { achv }, stats)
+      ok = pcall(achievement.new_achievements, achv, stats)
       assert.is_true(ok)
 
       local all_achv = achievement.get_all_achievements(stats)
       assert.is_equal(achv, all_achv[#all_achv])
+    end)
+
+    it('should fail on incomplete new achievement', function()
+      local get_stats = require('triforce.tracker').get_stats
+      local achv = {
+        {
+          id = 'test_achv_1',
+          desc = 'Test achievement #1',
+        },
+        {
+          desc = 'Test achievement #1',
+          check = function()
+            return true
+          end,
+        },
+      }
+      local ok, stats
+      ok, stats = pcall(get_stats)
+      assert.is_true(ok)
+
+      ok = pcall(achievement.new_achievements, achv, stats)
+      assert.is_false(ok)
+    end)
+
+    it('should fail new achievements on bad parameter types', function()
+      local get_stats = require('triforce.tracker').get_stats
+      local ok, stats
+      ok, stats = pcall(get_stats)
+      assert.is_true(ok)
+
+      ok = pcall(achievement.new_achievements, true, stats)
+      assert.is_false(ok)
     end)
   end)
 end)
